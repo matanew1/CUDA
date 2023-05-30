981314d29a152dc8cf088c605cd374522349bee7
linuxu <linuxu@ParallelC23-21.academic.management.afeka.local>
2023-05-30 23:38:30 +0300

    err

--- cudaFunctions.cu
+++ cudaFunctions.cu
@@ -2,37 +2,69 @@
 #include <helper_cuda.h>
 #include "myProto.h"
 
-__global__ void calculateHistogram(int* data, int* histogram, int* size) {
+__global__ void histogram_kernel(int* input, int* histogram, int *split_size) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    if (tid < *size) {
-        atomicAdd(&histogram[data[tid]], 1);
+    int stride = blockDim.x * gridDim.x;
+    
+    while (tid < (*split_size)) {
+        atomicAdd(&(histogram[input[tid]]), 1);
+        tid += stride;
     }
 }
 
-int computeOnGPU(int *data, int* split_size, int* histogram) {
 
-    int* dev_data;
-    int* dev_histogram;
+int computeOnGPU(int *local_array, int* split_size, int* hist) {
 
-        // Allocate memory on GPU
-    cudaMalloc((void**)&dev_data, (*split_size) * sizeof(int));
-    cudaMalloc((void**)&dev_histogram, NUM_BINS * sizeof(int));
+    // Error code to check return values for CUDA calls
+    cudaError_t err = cudaSuccess;
 
-    // Copy data from host to device
-    cudaMemcpy(dev_data, data, (*split_size) * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemset(dev_histogram, 0, NUM_BINS * sizeof(int));
+    // Allocate data on device
+    int* d_array = NULL;
+    int* d_hist = NULL;
 
-    // Launch CUDA kernels
-    int num_blocks = (*split_size + BLOCK_SIZE - 1) / BLOCK_SIZE;
-    calculateHistogram<<<num_blocks, BLOCK_SIZE>>>(dev_data, dev_histogram, split_size);
+    // Malloc device local_array and hist on device
+    err = cudaMalloc((void**)&d_array, (*split_size) * sizeof(int));
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error in line %d (error code %s)!\n", __LINE__, cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+    err = cudaMalloc((void**)&d_hist, RANGE * sizeof(int));
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error in line %d (error code %s)!\n", __LINE__, cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    //Copy data to the device
+    err = cudaMemcpy(d_array, local_array, (*split_size), cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) {
+      fprintf(stderr, "Error in line %d (error code %s)!\n", __LINE__, cudaGetErrorString(err));
+      exit(EXIT_FAILURE);
+    }
+
+    /**
+    Invalid MIT-MAGIC-COOKIE-1 keyInvalid MIT-MAGIC-COOKIE-1 keyError in line 49 (error code invalid argument)!
+    Error in line 49 (error code invalid argument)!
+    make: *** [Makefile:12: run] Error 1
+    */
+
+    // Initialize hist on device
+    histogram_kernel<<<NUM_BLOCKS, NUM_THREADS>>>(d_array, d_hist, split_size);
     
-    // Copy histogram from device to host
-    cudaMemcpy(histogram, dev_histogram, NUM_BINS * sizeof(int), cudaMemcpyDeviceToHost);
+    err = cudaMemcpy(hist, d_hist, (*split_size) * sizeof(int), cudaMemcpyDeviceToHost);
+    if (err != cudaSuccess) {
+      fprintf(stderr, "Error in line %d (error code %s)!\n", __LINE__, cudaGetErrorString(err));
+      exit(EXIT_FAILURE);
+    }
+
+    // Print the histogram array
+    for (int i = 0; i < (*split_size); i++) {
+        printf("Histogram[%d] = %d\n", i, hist[i]);
+    }
 
     // Free device global memory
-    cudaFree(dev_data);
-    cudaFree(dev_histogram);
-
-    printf("Done\n");
+    cudaFree(d_array);
+    cudaFree(d_hist);
+    printf("\nDone");
     return 0;
 }
+
